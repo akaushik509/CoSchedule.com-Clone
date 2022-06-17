ca355dd6cad2494062fe2e1deda2b4d258e26a29
naman kumar singh <singhnaman320@gmail.com>
2022-06-18 00:57:47 +0530

    pricing

---
+++ pricing.js
@@ -0,0 +1,30 @@
+// write your code here
+document.querySelector("#first").addEventListener("click", clickFirst);
+function clickFirst(){
+    window.location.href="signup.html"
+}
+
+document.querySelector("#second").addEventListener("click", clickSecond);
+function clickSecond(){
+    window.location.href="payment.html"
+}
+
+document.querySelector("#third").addEventListener("click", clickThird);
+function clickThird(){
+    window.location.href="signup.html"
+}
+
+// Extreme down box
+
+document.querySelector("#ek").addEventListener("click", pressone);
+
+function pressone(){
+    window.location.href="signup.html";
+}
+
+document.querySelector("#teen").addEventListener("click", presstwo);
+
+function presstwo(){
+    window.location.href="signIn.html";
+}
+
